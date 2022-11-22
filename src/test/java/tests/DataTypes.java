12baddaa7a84128d7cd158fcb94e7dd8ddd3b660
Natalya Bochkova <bochkovanatalya7@gmail.com>
2022-11-22 16:13:30 +0300

    Initial commit

---
+++ src/test/java/tests/DataTypes.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class DataTypes {
+    // Вычисления с разными типами данных
+    public static void main(String[] args) {
+            double a = 0.502;
+            int b = 2;
+            System.out.println(a*b);
+        }
+    }
+
---
+++ src/test/java/tests/OutofMemory.java
@@ -0,0 +1,11 @@
+package tests;
+
+//public class OutofMemory {
+    // Переполнение
+//    public static void main(String[] args) {
+//        byte a = 127;
+//        byte b = 5;
+//        byte sum = a + b;
+//        System.out.println(sum);
+//    }
+//}
