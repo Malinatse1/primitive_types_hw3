f5093168663f7b938a299883bd8a214aa4351b6c
Natalya Bochkova <bochkovanatalya7@gmail.com>
2022-11-25 13:41:20 +0300

    Fix types

--- src/test/java/tests/DataTypes.java
+++ src/test/java/tests/DataTypes.java
@@ -1,11 +1,27 @@
 package tests;
 
 public class DataTypes {
-    // Вычисления с разными типами данных
     public static void main(String[] args) {
-            double a = 0.502;
-            int b = 2;
-            System.out.println(a*b);
+        double myDouble = 0.502;
+        int myInt = 2;
+        byte myByte = 127;
+        float myFloat = 125.456f;
+        short myShort = -32768;
+        double multiplication = myDouble * myInt;
+        float multiplication2 = myFloat * myInt;
+        //     Переполнение
+
+        System.out.println(myByte + 100);
+        System.out.println(myShort - 1);
+
+        //     Вычисления с разными типами данных
+
+        if (multiplication >= multiplication2) {
+            System.out.println("multiplication больше");
+        } else if (multiplication2 >= multiplication) {
+            System.out.println("multiplication2 больше");
+
         }
     }
+}
 
--- src/test/java/tests/OutofMemory.java
+++ src/test/java/tests/OutofMemory.java
@@ -1,11 +1,9 @@
 package tests;
 
-//public class OutofMemory {
-    // Переполнение
-//    public static void main(String[] args) {
-//        byte a = 127;
-//        byte b = 5;
-//        byte sum = a + b;
-//        System.out.println(sum);
-//    }
-//}
+public class OutofMemory {
+//     Переполнение
+    public static void main(String[] args) {
+        byte a = 127;
+        System.out.println(a + 100);
+    }
+}
